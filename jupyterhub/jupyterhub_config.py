4bb942ada68482f5dea0cabf759e865b5ae84863
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2021-02-20 17:21:22 -0500

    Add extensions to jupyterlab

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -4,10 +4,15 @@ import sys
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_IMAGE']
 c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
+
+# https://jupyterlab.readthedocs.io/en/stable/user/jupyterhub.html
+c.DockerSpawner.cmd=["jupyter-labhub"]
 c.JupyterHub.hub_connect_ip = os.environ['HUB_IP']
 c.JupyterHub.hub_ip = "127.0.0.1"
 
-c.JupyterHub.admin_access = True
+# # For debugging
+# c.JupyterHub.authenticator_class = "jupyterhub.auth.DummyAuthenticator"
+# c.DummyAuthenticator.password = ""
 
 c.JupyterHub.services = [
     {
@@ -17,10 +22,11 @@ c.JupyterHub.services = [
     },
 ]
 
-c.Spawner.default_url = '/lab'
+# c.Spawner.default_url = '/lab'
 
 notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan/work'
 c.DockerSpawner.notebook_dir = notebook_dir
 c.DockerSpawner.volumes = {
     '/home/sidhu': '/home/jovyan/work',
 }
+c.DockerSpawner.debug = True
