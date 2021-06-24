f02ff9e1e00b87745a2ebd4cdb296e8388891889
first last <email_to_use>
2021-06-23 22:32:38 -0400

    mistakes were made

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -6,7 +6,7 @@ c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_IMAGE']
 c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
 
 # https://jupyterlab.readthedocs.io/en/stable/user/jupyterhub.html
-c.DockerSpawner.cmd=["jupyter-labhub"]
+# c.DockerSpawner.cmd=["jupyter-labhub"]
 c.JupyterHub.hub_connect_ip = os.environ['HUB_IP']
 c.JupyterHub.hub_ip = "127.0.0.1"
 
@@ -22,7 +22,7 @@ c.JupyterHub.services = [
     },
 ]
 
-# c.Spawner.default_url = '/lab'
+c.Spawner.default_url = '/lab'
 
 notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan/work'
 c.DockerSpawner.notebook_dir = notebook_dir
