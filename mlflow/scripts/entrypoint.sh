731f467798bdb66ec8fbeba587ec56528eb00743
Ashton Sidhu <ashton.sidhu1994@gmail.com>
2021-10-04 01:35:31 -0400

    Add a cd

--- mlflow/scripts/entrypoint.sh
+++ mlflow/scripts/entrypoint.sh
@@ -16,6 +16,8 @@ PORT="${PORT:-5000}"
 
 CONN_STRING="postgresql+psycopg2://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}${POSTGRES_EXTRAS}"
 
+cd $DEFAULT_ARTIFACT_ROOT;
+
 mlflow server \
     --backend-store-uri "${CONN_STRING}" \
     --default-artifact-root "file://${DEFAULT_ARTIFACT_ROOT}" \
